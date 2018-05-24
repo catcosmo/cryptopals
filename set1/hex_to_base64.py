bf7631bd67cc370e77d1aa2d3df4dde9661b00ce
catcosmo <astrangethought@hotmail.com>
2018-05-24 01:13:36 +0100

    almost there

--- set1/hex_to_base64.py
+++ set1/hex_to_base64.py
@@ -1,13 +1,16 @@
 hex_str = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-hex_str_decode = hex_str.decode('hex')
-hex_str_encode = hex_str_decode.encode('base64')
-# hex_bytes = bytes.fromhex(hex_str)
+
+# simple python solution:
+# hex_str_decode = hex_str.decode('hex')
+# hex_str_encode = hex_str_decode.encode('base64')
 # print hex_bytes
 
+# understanding hex:
 # hex_test = '6d'
-# ergebnis = 13 + 6*16
+# result = 13 + 6*16
 # print hex_test.decode('hex')
 
+base64_str = ''
 hex_alphabet = {'a': 10,
                 'b': 11,
                 'c': 12,
@@ -18,12 +21,12 @@ hex_alphabet = {'a': 10,
 
 # loop through hex string and write to bytes
 while not hex_str == '':
-    # take last two digits of hex code
+    # take first two digits of hex code
     hex_tuple = hex_str[:2]
-    # erase last two digits of hex code
+    # erase first two digits of hex code
     hex_str = hex_str[2:]
-    # decode first digit
     ascii_sum = 0
+    # decode first digit
     hex_one = hex_tuple[1]
     if hex_one in hex_alphabet:
         ascii_sum = hex_alphabet[hex_one]
@@ -35,5 +38,14 @@ while not hex_str == '':
         ascii_sum += hex_alphabet[hex_two]*16
     else:
         ascii_sum += int(hex_two)*16
-    print ascii_sum, chr(ascii_sum)
+    # debug: print ascii_sum, chr(ascii_sum)
+
+    # write ascii_sum to byte (not necessary for solution of challenge)
+    single_byte = ascii_sum.to_bytes(1, byteorder='big', signed=True)
+    print("{0:b}".format(ascii_sum))
+
+    # convert ascii_sum to base64
+    # lazy solution
+    base64_str += str(ascii_sum).encode('base64')
+
 
